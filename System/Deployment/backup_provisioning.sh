c395cb20ee1c530d9890d440c379a0a1f7724b94
a-enz <andi_enz@msn.com>
2017-10-25 12:04:02 +0200

    Some thoughts and comments on backup (and thus ssh) operation and setup

--- System/Deployment/backup_provisioning.sh
+++ System/Deployment/backup_provisioning.sh
@@ -1,1 +1,16 @@
 # TODO
+# Installing the software for rsync backup:
+# rsync: already on centos/7 box
+# ssh: already on centos/7 box
+# cron: already on centos/7 box
+
+# Basic idea: backup with rsync:
+# ==============================
+# 1) generate ssh keypair, push pubkey to backup server
+# 2) use rsync to push predefined files to backup server
+# 3) do scheduled backup with crontab
+# ssh-keygen create key (without passphrase)
+# ssh-copy-id user@hostname.example.com:  give public key to backup server
+# run crontab every 30min
+# */30 * * * * /usr/bin/somedirectory/somecommand
+# rsync -e ssh web_server vagrant@192.168.50.32:~/backup/
