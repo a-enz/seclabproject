345b2aef713b5af0b067b6d5498661053ed32df7
a-enz <andi_enz@msn.com>
2017-11-02 10:40:19 +0100

    Fixed escaping of echo command

--- System/Deployment/web_server_provisioning_nonpriv.sh
+++ System/Deployment/web_server_provisioning_nonpriv.sh
@@ -46,7 +46,7 @@ echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCWse17nPQ/8bYN+W2fg37ea/++Y6T6rTkod/
 touch ${DIR}backup.sh
 chmod u+x ${DIR}backup.sh
 touch ${DIR}backup.log
-echo "echo \"`date +%d-%m-%y/%H:%M:%S` starting backup process:\"" > ${DIR}backup.sh
+echo "echo \"\`date +%d-%m-%y/%H:%M:%S\` starting backup process:\"" > ${DIR}backup.sh
 echo "rsync -avz -e ssh example_dir vagrant@192.168.50.32:~/backup/" >> ${DIR}backup.sh
 
 echo "* * * * * ${DIR}backup.sh >> ${DIR}backup.log 2>&1" | crontab
