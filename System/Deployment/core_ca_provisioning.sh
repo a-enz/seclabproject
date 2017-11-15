963cbd2b8c40ae20d019bd28af401a45d1268aa5
Alessio Baehler <abaehler@student.ethz.ch>
2017-11-15 08:24:16 +0100

    modified deployment script

--- System/Deployment/core_ca_provisioning.sh
+++ System/Deployment/core_ca_provisioning.sh
@@ -14,5 +14,43 @@ export PATH="$PATH:$JAVA_HOME/bin"
 source /etc/environment
 
 # Set up CA stuff as showed at page 109 in the book
+echo "Setup"
+cd ~/
+
+# Clone repository
+ssh-keyscan -H gitlab.vis.ethz.ch >> ~/.ssh/known_hosts
+git clone git@gitlab.vis.ethz.ch:abaehler/SecLabProject.git
+
+# FIXME: Change branch until not merged
+git checkout rest_api
+
+# Copy relevant data here
+cp ./SecLabProject/System/Core\ CA/target .
+cp ./SecLabProject/System/Core\ CA/scripts .
+cp ./SecLabProject/System/Core\ CA/openssl.cnf .
+cp ./SecLabProject/System/Certificates/cacert.pem .
+cp ./SecLabProject/System/Certificates/cakey.pem .
+cp ./SecLabProject/System/Certificates/cakeystore .
+
+
 echo "Create CA directories and files"
-# TODO: copy from setup.sh
+mkdir ./ssl/CA
+mkdir ./ssl/CA/certs
+mkdir ./ssl/CA/newcerts
+mkdir ./ssl/CA/private
+mkdir ./ssl/CA/crl
+bash -c "echo '01' > ./ssl/CA/crlnumber"
+mkdir ./tmp
+mkdir ./logs
+bash -c "echo '01' > ./ssl/CA/serial"
+touch ./ssl/CA/index.txt
+mv ./cacert.pem ./ssl/CA
+mv ./cakey.pem ./ssl/CA/private
+mv ./openssl.cnf ./ssl
+
+
+# TODO: Clean up (logs, history, ...)
+rm -r ./SecLabProject
+
+# TODO: start application
+
